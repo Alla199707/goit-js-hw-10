a681fa11cfcd9a0448209a57454258b1c39631e2
Alla199707 <onoschenko20@gmail.com>
2023-02-11 14:12:45 +0200

    правки

--- src/index.js
+++ src/index.js
@@ -1,54 +1,71 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix';
-import fetchCountries from './js/fetch-countries';
+import { fetchCountries } from './js/fetch-countries';
 
 const DEBOUNCE_DELAY = 300;
 
 const countryInput = document.getElementById('search-box');
-const countryList = document.getSelection('country-list');
-const countryInfo = document.getSelection('country-info');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+console.log(countryInput);
 
 countryInput.addEventListener(
   'input',
   debounce(onCountryInput, DEBOUNCE_DELAY)
 );
 
-function onCountryInput() {
-  const name = countryInput.value.trim();
-
-  //   console.log(inputCurrent);
-  if (name === '') {
-    return (countryList.innerHTML = ''), (countryInfo.innerHTML = '');
+function onCountryInput(e) {
+  const name = e.target.value.trim();
+  console.log(name);
+  if (name.length === 1) {
+    alertTooManyMatches();
+  } else if (name.length === 2) {
+    fetchCountries(name).then(renderCountryList).catch(alertWrongName);
+  } else if (name.length >= 3) {
+    fetchCountries(name).then(renderCountryInfo).catch(alertWrongName);
+  } else if (name === '') {
+    onClear();
   }
-  fetchCountries(name)
-    .then(countries => {
-      countryList.innerHTML = '';
-      countryInfo.innerHTML = '';
-      if (countries.length === 1) {
-        countryList.insertAdjacentHTML(
-          'beforeend',
-          renderCountryList(countries)
-        );
-        countryInfo.insertAdjacentHTML(
-          'beforeend',
-          renderCountryInfo(countries)
-        );
-      } else if (countries.length >= 10) {
-        alertTooManyMatches();
-      } else {
-        countryList.insertAdjacentHTML(
-          'beforeend',
-          renderCountryList(countries)
-        );
-      }
-    })
-    .catch(alertWrongName);
 }
 
-function renderCountryList(countries) {}
+function renderCountryList(countries) {
+  onClear();
+  const markup = countries
+    .map(({ name, flags }) => {
+      return `
+          <li class="list country-list__item">
+              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+              <h2 class="country-list__name">${name.official}</h2>
+          </li>
+          `;
+    })
+    .join('');
+  countryList.insertAdjacentHTML('beforeend', markup);
+}
 
-function renderCountryInfo(countries) {}
+function renderCountryInfo(countries) {
+  onClear();
+  const markup = countries
+    .map(({ name, capital, flag, population, languages }) => {
+      const nameLanguages = languages.map(language => language.name);
+      return `
+        <div class="country_title">
+            <img src='${flag}' width="50" height="30"/>
+            <h1 class="title"><b>${name}</b></h1>
+        </div>
+        <ul class="list country-info__list">
+            <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+            <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+            <li class="country-info__item"><p><b>Languages: </b>${nameLanguages.join(
+              ', '
+            )}</p></li>
+        </ul>
+        `;
+    })
+    .join('');
+  countryInfo.insertAdjacentHTML('beforeend', markup);
+}
 
 function alertTooManyMatches() {
   Notify.info('Too many matches found. Please enter a more specific name.');
@@ -56,4 +73,9 @@ function alertTooManyMatches() {
 
 function alertWrongName() {
   Notify.failure('Oops, there is no country with that name');
+}
+
+function onClear() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
 }
--- src/js/fetch-countries.js
+++ src/js/fetch-countries.js
@@ -1,10 +1,15 @@
-const BASE_URL = 'https://restcountries.com/v3.1/name/';
-const fields = 'fields=name,capital,population,flags,languages';
+// const BASE_URL = 'https://restcountries.com/v3.1/name/';
+// const fields = 'fields=name,capital,population,flags,languages';
 
-function fetchCountries(name) {
-  return fetch(`${BASE_URL}${name}?${fields}`)
-    .then(response => response.json())
-    .catch(error => console.log(error));
-}
-// console.log(fetchCountries);
-export default fetchCountries;
+// function fetchCountries(name) {
+//   return fetch(`${BASE_URL}${name}?${fields}`)
+//     .then(response => response.json())
+//     .catch(error => console.log(error));
+// }
+// // console.log(fetchCountries);
+// export default fetchCountries;
+
+export const fetchCountries = name => {
+  return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages
+  `).then(response => response.json());
+};
